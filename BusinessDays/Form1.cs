a15c95724fb5cd0404fbfa9723cf566fbb021725
Ramon N. Gene <RNGene@hotmail.com>
2010-07-29 11:25:22 -0400

    first commit

---
+++ BusinessDays/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BusinessDays
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            label1.Text = AddBusinessDays(Convert.ToDateTime(dateTimePicker1.Text), 30).ToString();
+        }
+
+
+        private DateTime AddBusinessDays(DateTime date, int days)
+        {
+            while (days > 0)
+            {
+                date=date.AddDays(1);
+                if (date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday)
+                    continue;
+                else
+                    days--;
+            }
+            return date;
+        }
+    }
+}
